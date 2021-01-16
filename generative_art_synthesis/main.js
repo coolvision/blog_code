c3818450640449c3c90d01ca715797efb590329e
sk <sk@sks-MacBook-Pro.lan>
2021-01-16 21:07:04 +0200

    better ui, now will make sure stepping is working

--- generative_art_synthesis/main.js
+++ generative_art_synthesis/main.js
@@ -13,6 +13,10 @@ var time_delay = 5;
 
 function generate() {
 
+	if (!generating) {
+		state.iter = 0;
+	}
+
 	// console.log("generate", stop_processing, demo_id)
 	generating = true;
 
@@ -24,10 +28,10 @@ function generate() {
 	}
 
 	setTimeout(function(){
-		for (let i = 0; i < iterations; i++) {
-			if (stop_processing) break;
+		// for (let i = 0; i < iterations; i++) {
+			// if (stop_processing) break;
 			next();
-		}
+		// }
 		generate();
 	}, time_delay);
 }
@@ -41,6 +45,7 @@ function reset() {
 	let N = 500;
 
 	// let state = {};
+	state.N = N;
 	state.options_track = new Array(N).fill(-1, 0, N);
 	state.alt_n = new Array(N).fill(-1, 0, N);
 	state.filter = new Array(N).fill([], 0, N);
@@ -53,8 +58,7 @@ function reset() {
 	state.programs_n = 0;
 	state.iteration = 0;
 	state.total = -1;
-	state.iter = 0;
-	// console.log("reset", state);
+	console.log("reset", state);
 }
 
 function next(show = false) {
@@ -68,25 +72,67 @@ function next(show = false) {
 
 function findValidProgram(state, task, show = false) {
 
-	state.iteration++;
+	// state.iteration++;
+	//
+	// let prev_program = state.program;
+	// let next_result = next_program_cached(task, state, true);
+	//
+	// let new_program = next_result["program"];
+	//
+	// if (show) {
+	// 	let javascript = json2js(JSON.stringify(new_program), [], true);
+	// 	$("#js_code").text(javascript);
+	// 	$("#test_code").text(JSON.stringify(new_program));
+	// }
 
-	let prev_program = state.program;
-	let next_result = next_program_cached(task, state, true);
 
-	let new_program = next_result["program"];
+	// task = JSON.parse(dsl_config);
+	// task.add_random = true;
+	// task.count_all_placeholders = true;
+	//
+	// let N = 500;
+	//
+	// state = {};
+	// state.options_track = new Array(N).fill(-1, 0, N);
+	// state.alt_n = new Array(N).fill(-1, 0, N);
+	// state.filter = new Array(N).fill([], 0, N);
+	// state.programs = new Array(N).fill([], 0, N);
+	// state.js_programs = new Array(N).fill("", 0, N);
+	// task.depth_limit = 0;
+	//
+	// state.program = task.template;
+	// state.step = 0;
+	// state.programs_n = 0;
+	// state.iteration = 0;
+	// state.total = -1;
 
-	if (show) {
-		let javascript = json2js(JSON.stringify(new_program), [], true);
-		$("#js_code").text(javascript);
-		$("#test_code").text(JSON.stringify(new_program));
+	// console.log("findValidProgram", state.iter, stop_processing, generating, state.N);
+
+	let next_result = {};
+	let program = [];
+	let new_program = [];
+	for (let i = 0; i < state.N; i++) {
+
+		next_result = next_program_cached(task, state, true);
+
+		new_program = next_result["program"];
+
+		if (new_program.length > 0 && next_result.filter["placeholders_n"] == 0) {
+			// let javascript = json2js(JSON.stringify(new_program), [], true);
+			// console.log("new_program", next_result, javascript)
+			program = new_program;
+			break;
+		}
 	}
+
+
 
 	if (new_program.length > 0 && next_result.filter["placeholders_n"] == 0) {
 
 		let script_string = JSON.stringify(new_program);
 
 		let javascript = json2js(script_string, [], true);
-		// console.log("new_program", next_result, javascript)
+		// console.log("2 new_program", next_result, javascript)
 		program = new_program;
 
 		set_count_objects = true;
@@ -95,7 +141,7 @@ function findValidProgram(state, task, show = false) {
 
 		$("#js_code").text(javascript);
 		$("#test_code").text(script_string);
-		// state.iter++;
+		state.iter++;
 		$("#iter_progress").text(state.iter);
 		$("#iter_progress1").text("");
 
@@ -104,7 +150,7 @@ function findValidProgram(state, task, show = false) {
 			objects_n = getObjectsNumber(renderer_B);
 		}
 
-		// console.log("rendered", rendered, objects_n)
+		// console.log("rendered", rendered, objects_n, state.iter)
 
 		if (rendered && objects_n >= 10) {
 		// if (rendered) {
